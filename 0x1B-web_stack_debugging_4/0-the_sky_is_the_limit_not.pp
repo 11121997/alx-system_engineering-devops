93fb2b46cdcef7b13897872f36710bc16d376cf6
11121997 <nawalkhaled27@yahoo.com>
2024-05-16 16:05:27 +0000

    0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+#testing how well our web server setup featuring Nginx is doing under pressure
+#fix our stack so that we get to 0
+
+exec { 'fix--for-nginx':
+  command => '/usr/bin/env sed -i s/15/2000/ /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
