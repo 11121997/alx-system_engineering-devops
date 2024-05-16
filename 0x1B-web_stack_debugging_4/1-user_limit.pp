6d5c0cabd6469b97ce68a6f7004688519228b82e
11121997 <nawalkhaled27@yahoo.com>
2024-05-16 16:16:20 +0000

    0

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,4 @@
+#Change the OS configuration so that it is possible to login with the holberton user 
+#and open a file without any error message
+
+exec { '/usr/bin/env sed -i "s/4/20000/; s/5/20000/" /etc/security/limits.conf' }
